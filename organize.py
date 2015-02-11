e29be19a51a9d3a2281e1c62967bbca1f26d1f24
Davide Tampellini <tampe125@gmail.com>
2015-02-11 21:57:46 +0100

    Added script for classifying files and move them accordingly

--- organize.py
+++ organize.py
@@ -1,42 +1,47 @@
 __author__ = 'tampe125'
 
+import os
+import csv
 import scipy as sp
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
+import shutil
 from sklearn import neighbors
 
-data = sp.genfromtxt("training/features.csv", delimiter=",", skip_header=1, usecols=(0, 2))
-target = sp.genfromtxt("training/features.csv", delimiter=",", skip_header=1, usecols=(-2))
+__DIR__ = os.path.dirname(os.path.realpath(__file__))
+dir_organized = __DIR__ + "/data/organized/"
+
+training = sp.genfromtxt(__DIR__ + "/training/features.csv", delimiter=",", skip_header=1, usecols=(0, 1, 2))
+target = sp.genfromtxt(__DIR__ + "/training/features.csv", delimiter=",", skip_header=1, usecols=(-2))
 
 n_neighbors = 10
-h = .02  # step size in the mesh
 
-cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
-cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
-
+# Let's load the training data into the classifier
 clf = neighbors.KNeighborsClassifier(n_neighbors, weights='uniform')
+clf.fit(training, target)
 
-clf.fit(data, target)
+data = sp.genfromtxt(__DIR__ + "/data/features.csv", delimiter=",", skip_header=1)
 
-# Plot the decision boundary. For that, we will assign a color to each
-# point in the mesh [x_min, m_max]x[y_min, y_max].
-x_min, x_max = data[:, 0].min() - 1, data[:, 0].max() + 1
-y_min, y_max = data[:, 1].min() - 1, data[:, 1].max() + 1
+with open(__DIR__ + "/data/features.csv", 'rb') as csvfile:
+    reader = csv.reader(csvfile)
 
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    for line in reader:
+        if line[0] == 'Trash score':
+            continue
 
-Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+        features = line[0:3]
+        label = clf.predict(features)
 
-# Put the result into a color plot
-Z = Z.reshape(xx.shape)
-plt.figure()
-plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+        if label == 0:
+            folder = 'trash'
+        elif label == 1:
+            folder = 'hash'
+        elif label == 2:
+            folder = 'plain'
 
-# Plot also the training points
-plt.scatter(data[:, 0], data[:, 1], c=target, cmap=cmap_bold)
-plt.xlim(xx.min(), xx.max())
-plt.ylim(yy.min(), yy.max())
-plt.title("3-Class classification (k = %i, weights = '%s')" % (n_neighbors, 'uniform'))
+        target_dir = dir_organized + folder + "/" + line[-1]
 
-plt.show()
+        if not os.path.exists(os.path.dirname(target_dir)):
+            os.makedirs(os.path.dirname(target_dir))
+
+        shutil.copyfile(__DIR__ + "/data/" + line[-1], target_dir)
+
+        print(str(line[-1]) + " Label: " + folder.capitalize())
