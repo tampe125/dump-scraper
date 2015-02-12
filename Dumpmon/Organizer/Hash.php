1c8d3321ca0589cfa04ff5f73de19462c0be7b27
Davide Tampellini <tampe125@gmail.com>
2015-02-12 09:14:18 +0100

    Added more hashes

--- Dumpmon/Organizer/Hash.php
+++ Dumpmon/Organizer/Hash.php
@@ -6,8 +6,6 @@ class Hash extends Organizer
 {
     public function analyze()
     {
-        $hashes = $this->detectMd5();
-
         // If I just have few lines, most likely it's trash. I have to do this since sometimes some debug output are
         // crammed into a single line, screwing up all the stats
         if($this->lines < 3)
@@ -16,27 +14,35 @@ class Hash extends Organizer
         }
         else
         {
+            $hashes  = $this->detectMd5();
+            $hashes += $this->detectMd5Crypt();
+            $hashes += $this->detectSha1();
+            $hashes += $this->detectMySQL();
+
             $this->score = $hashes / $this->lines;
         }
     }
 
     protected function detectMd5()
     {
-        return (int) preg_match_all('/[a-f0-9]{32}/i', $this->data);
+        return (int) preg_match_all('/[a-f0-9]{32}/im', $this->data);
     }
 
     protected function detectMd5Crypt()
     {
         // Example (unsalted) $1$sCGfZOwq$K9M3ULuacSQln/e3/KnPN.
+        return preg_match_all('/\$1\$.{8}\$.{22}/im', $this->data);
     }
 
     protected function detectSha1()
     {
         // Regex \b[0-9a-f]{40}\b
+        return preg_match_all('/\b[0-9a-f]{40}\b/im', $this->data);
     }
 
     protected function detectMySQL()
     {
         // Regex /\*[a-f0-9]{40}/i
+        return preg_match_all('/\*[a-f0-9]{40}/im', $this->data);
     }
 }
