3fd0d05dc3fe1ed75cc89f625af3d4b5712557be
Davide Tampellini <tampe125@gmail.com>
2015-02-14 22:43:39 +0100

    Extract hashes from files

--- Dumpmon/Extractor/Hash.php
+++ Dumpmon/Extractor/Hash.php
@@ -6,7 +6,39 @@ class Hash extends Extractor
 {
     public function analyze()
     {
+        $data  = '';
 
+        $data .= $this->extractPhpassGen()."\n";
+        $data .= $this->extractMd5()."\n";
+        $data .= $this->extractCrypt()."\n";
+
+        $this->extracted = $data;
     }
 
+    protected function extractPhpassGen()
+    {
+        $this->matches = array();
+
+        $this->data = preg_replace_callback('/(\$P\$.{31})/im', array($this, 'replaceMatches'), $this->data);
+
+        return implode("\n", $this->matches);
+    }
+
+    protected function extractMd5()
+    {
+        $this->matches = array();
+
+        $this->data = preg_replace_callback('/([a-f0-9]{32})/im', array($this, 'replaceMatches'), $this->data);
+
+        return implode("\n", $this->matches);
+    }
+
+    protected function extractCrypt()
+    {
+        $this->matches = array();
+
+        $this->data = preg_replace_callback('/([a-z0-9\/\.]{13})[,\s\n]?$/im', array($this, 'replaceMatches'), $this->data);
+
+        return implode("\n", $this->matches);
+    }
 }
