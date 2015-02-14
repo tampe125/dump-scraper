eae5f2399f4140743df7d5ba2b661bdd394b23ff
Davide Tampellini <tampe125@gmail.com>
2015-02-14 14:13:21 +0100

    Refactored Hash detector

--- Dumpmon/Detector/Hash.php
+++ Dumpmon/Detector/Hash.php
@@ -4,80 +4,134 @@ namespace Dumpmon\Detector;
 
 class Hash extends Detector
 {
+    private $functions;
+
+    public function __construct()
+    {
+        $this->functions = array(
+            'fewLines'       => 1,
+            'longLines'      => 1,
+            'detectMd5'      => 1,
+            'detectMd5Crypt' => 1,
+            'phpassMd5'      => 1,
+            'phpassGen'      => 1,
+            'detectSha1'     => 1,
+            'detectMySQL'    => 1,
+            'detectDrupal'   => 1,
+        );
+    }
+
     public function analyze()
     {
-        // If I just have few lines, most likely it's trash. I have to do this since sometimes some debug output are
-        // crammed into a single line, screwing up all the stats
-        if($this->lines < 3)
+        foreach($this->functions as $method => $coefficient)
         {
-            $this->score = 0;
-        }
-        else
-        {
-            // Let's check for line length. I have to duplicate the check here otherwise I'll get abnormous values
-            $lines = explode("\n", $this->data);
-
-            foreach($lines as $line)
+            if(method_exists($this, $method))
             {
-                // Line too long? Ok, set the score to zero and stop here
-                if(strlen($line) > 1000)
+                $score = $this->$method() * $coefficient;
+
+                // Did I get a negative score? This means that this file is NOT an hash one!
+                // Set the global score to 0 and stop here
+                if($score < 0)
                 {
                     $this->score = 0;
-
-                    return ;
+                    break;
                 }
+
+                $this->score += $score;
             }
 
-            $hashes  = $this->detectMd5();
-            $hashes += $this->detectMd5Crypt();
-            $hashes += $this->phpassMd5();
-            $hashes += $this->phpassGen();
-            $hashes += $this->detectSha1();
-            $hashes += $this->detectMySQL();
-            $hashes += $this->detectDrupal();
-
-            $this->score = $hashes / $this->lines;
+            // I already reached the maximum value, there's no point in continuing
+            if($this->score >= 3)
+            {
+                break;
+            }
         }
     }
 
     protected function detectMd5()
     {
-        return (int) preg_match_all('/[a-f0-9]{32}/im', $this->data);
+        $hashes = (int) preg_match_all('/[a-f0-9]{32}/im', $this->data);
+
+        return $hashes / $this->lines;
     }
 
     protected function detectMd5Crypt()
     {
         // Example (unsalted) $1$sCGfZOwq$K9M3ULuacSQln/e3/KnPN.
-        return preg_match_all('/\$1\$.{8}\$.{22}/im', $this->data);
+        $hashes = preg_match_all('/\$1\$.{8}\$.{22}/im', $this->data);
+
+        return $hashes / $this->lines;
     }
 
-    private function phpassMd5()
+    protected function phpassMd5()
     {
         // Example $H$9V1cX/WqUhsSWM0ipyB7HwFQqTQKxP1
-        return preg_match_all('/\$H\$9.{30}/m', $this->data);
+        $hashes = preg_match_all('/\$H\$9.{30}/m', $this->data);
+
+        return $hashes / $this->lines;
     }
 
-    private function phpassGen()
+    protected function phpassGen()
     {
         // Example $P$B52zg0z/Y5e96IpD4KJ7a9ByqcrKb01
-        return preg_match_all('/\$P\$.{31}/m', $this->data);
+        $hashes = preg_match_all('/\$P\$.{31}/m', $this->data);
+
+        return $hashes / $this->lines;
     }
 
     protected function detectSha1()
     {
         // Regex \b[0-9a-f]{40}\b
-        return preg_match_all('/\b[0-9a-f]{40}\b/im', $this->data);
+        $hashes = preg_match_all('/\b[0-9a-f]{40}\b/im', $this->data);
+
+        return $hashes / $this->lines;
     }
 
     protected function detectMySQL()
     {
         // Regex /\*[a-f0-9]{40}/i
-        return preg_match_all('/\*[a-f0-9]{40}/im', $this->data);
+        $hashes = preg_match_all('/\*[a-f0-9]{40}/im', $this->data);
+
+        return $hashes / $this->lines;
     }
 
-    private function detectDrupal()
+    protected function detectDrupal()
     {
         // Drupal $S$DugG4yZmhfIGhNJJZMzKzh4MzOCkpsPBR9HtDIvqQeIyqLM6wyuM
-        return preg_match_all('/\$S\$.{52}/m', $this->data);
+        $hashes =  preg_match_all('/\$S\$.{52}/m', $this->data);
+
+        return $hashes / $this->lines;
+    }
+
+    /**
+     * Files with huge lines are debug info
+     *
+     * @return int
+     */
+    protected function longLines()
+    {
+        $lines = explode("\n", $this->data);
+
+        foreach($lines as $line)
+        {
+            if(strlen($line) > 1000)
+            {
+                return -1;
+            }
+        }
+
+        return 0;
+    }
+
+    protected function fewLines()
+    {
+        // If I just have few lines, most likely it's trash. I have to do this since sometimes some debug output are
+        // crammed into a single line, screwing up all the stats
+        if($this->lines < 3)
+        {
+            return -1;
+        }
+
+        return 0;
     }
 }
