2a0ec692a41c686c07a4964b85b79465582da6ce
Davide Tampellini <tampe125@gmail.com>
2015-05-31 11:29:28 +0200

    Added more checks links and contents

--- src/lib/runner/scrapeold.py
+++ src/lib/runner/scrapeold.py
@@ -16,7 +16,7 @@ from lib.exceptions.exceptions import RunningError
 
 class DumpScraperScrapeold(AbstractCommand):
     def run(self):
-        base_url = 'https://twitter.com/i/search/timeline?q='
+        base_url = 'https://twitter.com/i/search/timeline?f=realtime&q='
         base_query = 'from:dumpmon since:%s until:%s'
         prev_day = '1970-05-01'
         processed = 0
@@ -29,11 +29,21 @@ class DumpScraperScrapeold(AbstractCommand):
         while processing:
             r = requests.get(url)
             json_data = json.loads(r.content)
-            html = LH.fromstring(json_data['items_html'])
+            raw_html = json_data['items_html'].strip()
+
+            if not raw_html:
+                processing = False
+                continue
+
+            html = LH.fromstring(raw_html)
 
             removed = 0
+            tweets = html.cssselect('.original-tweet')
 
-            for tweet in html.cssselect('.original-tweet'):
+            if not tweets:
+                processing = False
+
+            for tweet in tweets:
                 link = tweet.cssselect('.twitter-timeline-link')
 
                 if not link:
@@ -45,6 +55,9 @@ class DumpScraperScrapeold(AbstractCommand):
                 paste_link = link.get('data-expanded-url')
                 timestamp = tweet.cssselect('.js-short-timestamp')[0].get('data-time')
                 tweetid = tweet.get('data-tweet-id')
+
+                if not paste_link:
+                    continue
 
                 day = datetime.datetime.fromtimestamp(float(timestamp)).strftime('%Y-%m-%d')
 
@@ -60,7 +73,11 @@ class DumpScraperScrapeold(AbstractCommand):
 
                 sleep(self.settings['delay'])
 
-                data = requests.get(paste_link)
+                # Sometimes we download virus and the AV drops the connection
+                try:
+                    data = requests.get(paste_link)
+                except requests.exceptions.ConnectionError:
+                    continue
 
                 if not data.text:
                     continue
@@ -82,11 +99,8 @@ class DumpScraperScrapeold(AbstractCommand):
                 with open(os.path.realpath("data/raw/" + folder + "/" + str(tweetid) + ".txt"), 'w+') as dump_file:
                     dump_file.write(data.text.encode('utf-8'))
 
-            processing = json_data['has_more_items']
-
-            print("")
-            print("\tprocessed " + str(processed) + " tweets")
-            print("\tFound " + str(removed) + " removed tweets in this batch")
+            # Let's setup the url for the next iteration
+            url = origurl + '&scroll_cursor=' + json_data['scroll_cursor']
 
         print("")
         print("Total processed tweets: " + str(processed))
